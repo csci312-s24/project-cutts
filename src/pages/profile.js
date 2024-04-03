7ec6601c3f8e52b7b4f785045f8a602f843890c8
cbbaxter <catiebbaxter@icloud.com>
2024-04-03 13:26:24 -0400

    added profile buttons to driver/rider and home to all pages for navigation

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,3 +1,23 @@
+import styles from "@/styles/Home.module.css";
+import { Inter } from "next/font/google";
+import { useRouter } from "next/router";
+
+const inter = Inter({ subsets: ["latin"] });
+
 export default function Profile() {
-  return <div>Profile Information</div>;
+  const router = useRouter();
+  return (
+    <div>
+      <button
+        type="button"
+        className={styles.card}
+        onClick={() => router.push("/")}
+      >
+        <h2 className={inter.className}>
+          Home <span>-&gt;</span>
+        </h2>
+      </button>
+      <p>Profile Information</p>
+    </div>
+  );
 }
--- src/pages/rider.js
+++ src/pages/rider.js
@@ -1,3 +1,32 @@
+import styles from "@/styles/Home.module.css";
+import { Inter } from "next/font/google";
+import { useRouter } from "next/router";
+
+const inter = Inter({ subsets: ["latin"] });
+
 export default function Rider() {
-  return <div>View / create Planned Trips</div>;
+  const router = useRouter();
+  return (
+    <div>
+      <button
+        type="button"
+        className={styles.card}
+        onClick={() => router.push("/profile")}
+      >
+        <h2 className={inter.className}>
+          Profile <span>-&gt;</span>
+        </h2>
+      </button>
+      <button
+        type="button"
+        className={styles.card}
+        onClick={() => router.push("/")}
+      >
+        <h2 className={inter.className}>
+          Home <span>-&gt;</span>
+        </h2>
+      </button>
+      <p>Planned Trips</p>
+    </div>
+  );
 }
