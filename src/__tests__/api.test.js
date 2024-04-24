e1f7e82c5a418ac8dfc53e629a2b3e3f3cc39d6a
Lila Kosowsky <lkosowsky@middlebury.edu>
2024-04-24 10:37:03 -0400

    first api test works

--- src/__tests__/api.test.js
+++ src/__tests__/api.test.js
@@ -10,34 +10,34 @@ import { knex } from "../../knex/knex";
 import plannedTripEndpoint from "../pages/api/plannedTrip/index";
 
 describe("Cutts API", () => {
+  beforeAll(
+    () =>
+      // ensure test database is initialized before tests
+      knex.migrate.rollback().then(() => knex.migrate.latest()),
+    20000,
+  );
 
-    beforeAll(
-        () => 
-        // ensure test database is initialized before tests 
-        knex.migrate.rollback().then(() => knex.migrate.latest()), 20000  
-    );
+  afterAll(() =>
+    // ensure database collection is cleaned up after all tests
+    knex.destroy(),
+  );
 
-    afterAll(
-        () => 
-        // ensure database collection is cleaned up after all tests
-        knex.destroy(),
-    );
+  beforeEach(
+    () =>
+      // reset contents of test database
+      knex.seed.run(),
+    20000,
+  );
 
-    beforeEach(
-        () =>
-        // reset contents of test database 
-        knex.seed.run(), 20000
-    );
-
-    test("GET /api/plannedTrip should return all trips with dates that haven't happened yet", async () => {
-        await testApiHandler({
-            rejectOnHandlerError: true, // Make sure to catch any errors
-            pagesHandler: plannedTripEndpoint, // NextJS API function to test
-            test: async ({ fetch }) => {
-                // Test endpoint with mock fetch
-                const res = await fetch();
-                // decide how to implement this test 
-            }
-        })
-    })
-})
+  test("GET /api/plannedTrip should return all trips with dates that haven't happened yet", async () => {
+    await testApiHandler({
+      rejectOnHandlerError: true, // Make sure to catch any errors
+      pagesHandler: plannedTripEndpoint, // NextJS API function to test
+      test: async ({ fetch }) => {
+        // Test endpoint with mock fetch
+        const res = await fetch();
+        await expect(res.json()).resolves.toHaveLength(2);
+      },
+    });
+  });
+});
