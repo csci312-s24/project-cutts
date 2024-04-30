4f90c6d9f24687e3be2c2c1071a9478dd0c7c946
Devon Tyrie <104871527+devontyr@users.noreply.github.com>
2024-04-30 18:52:00 -0400

    User GET method... PUT needs help

--- src/pages/profile/edit.js
+++ src/pages/profile/edit.js
@@ -1,27 +1,32 @@
 import { useRouter } from "next/router";
-import { createRouter } from "next-connect";
+import { useSession } from "next-auth/react";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import { useEffect, useState } from "react";
 import AppUserProfileEdit from "../../components/AppUserProfileEdit";
 import theme from "../../material/theme";
 
 export default function EditProfile() {
   const router = useRouter();
+  const { data: session } = useSession();
+  const [localUser, setLocalUser] = useState(null);
+  console.log(session);
 
-  let localUser;
-  fetch("/api/User")
-    .then((resp) => resp.json())
-    .then((data) => {
-      localUser = data;
-    });
+  useEffect(() => {
+    if (!session) return;
+    fetch(`/api/User/${session.user.id}`)
+      .then((res) => res.json())
+      .then((data) => setLocalUser(data));
+  }, [session]);
 
-  const complete = async (User) => {
-    const createdRouter = createRouter();
-    if (User) {
-      createdRouter.put(async (req, res) => {
-        const { id, ...userInfo } = req.body;
-        const user = await User.query().updateAndFetchById(id, userInfo);
-        res.status(200).json(user);
+  const complete = async (thisUser) => {
+    if (thisUser) {
+      fetch(`/api/User/${thisUser.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(thisUser),
       });
       router.back();
     } else {
@@ -32,7 +37,9 @@ export default function EditProfile() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <AppUserProfileEdit appUser={localUser} complete={complete} />
+      {localUser && (
+        <AppUserProfileEdit appUser={localUser} complete={complete} />
+      )}
     </ThemeProvider>
   );
 }
