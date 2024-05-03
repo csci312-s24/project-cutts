a333ab7ba59c5f1871de785c179fb81d90361323
Colman Bashore <cbashore@middlebury.edu>
2024-05-03 14:02:25 -0400

    landing page

--- src/pages/rider/index.js
+++ src/pages/rider/index.js
@@ -4,8 +4,7 @@ import { useRouter } from "next/router";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
-import HomeIcon from "@mui/icons-material/Home";
-import theme, { ProfileButton, HomeButton, Footer } from "../../material/theme";
+import theme, { ProfileButton, Footer } from "../../material/theme";
 
 export default function Rider() {
   const router = useRouter();
@@ -23,14 +22,6 @@ export default function Rider() {
       >
         Profile
       </ProfileButton>
-      <HomeButton
-        variant="outlined"
-        size="medium"
-        endIcon={<HomeIcon />}
-        onClick={() => handleClick("")}
-      >
-        Home
-      </HomeButton>
       <Container sx={{ mt: 10 }}>
         <Typography variant="h4" align="left" sx={{ color: "#0C4C7F" }}>
           Rider Portal
